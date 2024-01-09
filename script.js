77e75a2299206934a4f30fb820caeaeaa5f2371a
Mohammed Rahman <mohammed.rahman676@gmail.com>
2024-01-09 18:08:58 +0000

    added psuedocode

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// 
+// Allows user to edit event for each hour of day
+// Uses JQuery
+// Using Day.js to work with date & time
+// current day at the top of calendar when user opens calendar
+// present time blocked out for standard business hours
+// Color Code each time block based on past, present and future
+// Allow user to enter event when they click on time block
+// Save event in local storage when the save button is clicked
+
+
+
